0c442d1369cbcdcabbb715be07af86bf699a9ecd
Alex Balatsky <balatskiy337@gmail.com>
2021-06-08 20:55:11 +0300

    Add Surf Wallet support

---
+++ src/utils/contracts/SurfWallet/index.js
@@ -0,0 +1,16 @@
+import tvc from './SurfWallet.tvc'
+import abi from './SurfWallet.abi.json'
+import { defaultMultisigDeployParams, defaultMultisigTransferParams } from '../defaultParams'
+
+export default {
+  abi,
+  tvc,
+  slug: 'SurfWallet',
+  hash: '207dc560c5956de1a2c1479356f8f3ee70a59767db2bf4788b1d61ad42cdad82',
+  getDeployParams: (keys) => defaultMultisigDeployParams({ abi, tvc }, keys),
+  getTransferParams: ({ data, wallet }) => defaultMultisigTransferParams({
+    data,
+    wallet,
+    contract: { abi, tvc }
+  })
+}
--- src/utils/contracts/index.js
+++ src/utils/contracts/index.js
@@ -1,9 +1,10 @@
+import SurfWallet from './SurfWallet'
 import SafeMultisig from './SafeMultisigWallet'
 import SetcodeMultisigWallet from './SetcodeMultisigWallet'
 import InternalTransferContract from './InternalTransfer'
 
 export const InternalTransfer = InternalTransferContract
-export const wallets = [SafeMultisig, SetcodeMultisigWallet]
+export const wallets = [SafeMultisig, SetcodeMultisigWallet, SurfWallet]
 
 export const getContractByHash = hash => {
   return wallets.find(w => w.hash === hash)
